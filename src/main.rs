81d6df9400e3ea66c2e042c38ea1a4e5fd3f908a
maxscherbakov <max-tsh@yandex.ru>
2024-09-26 19:23:06 +0300

    vector sort

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+use itertools::Itertools;
+use std::io;
 
+fn main() {
+    let mut input_line = String::new();
+    io::stdin()
+        .read_line(&mut input_line)
+        .expect("Failed to read line");
+    let mut x: Vec<i32> = input_line
+        .split(' ')
+        .map(|x| x.trim().parse().expect("Not Int"))
+        .collect();
+    x.sort();
+    println!("{}", Itertools::join(&mut x.iter(), " "));
 }
